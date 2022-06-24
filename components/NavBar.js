54c132b0346bb8573eb33ff9eb7a7fac67b8dce5
RootZero <rootzero17@gmail.com>
2022-06-25 02:42:43 +0900

    styles jsx 코드 예제 추가

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css";
 
 const NavBar = () => {
 
@@ -9,15 +8,22 @@ const NavBar = () => {
     return (
         <nav>
         <Link href="/">
-            <a className={`
-                ${styles.link} 
-                ${router.pathname === "/" ? styles.active : ''}`}>Home</a>
+            <a className={router.pathname === "/" ? "active" : ""}>Home</a>
         </Link>
         <Link href="/about">
-            <a className={`
-                ${styles.link} 
-                ${router.pathname === "/about" ? styles.active : ''}`}>About</a>
+            <a className={router.pathname === "/about" ? "active" : ""}>About</a>
         </Link>
+        <style jsx>{`
+            nav {
+                background-color: tomato;
+            }
+            a {
+                text-decoration: none;
+            }
+            .active {
+                color: blue;
+            }
+        `}</style>
     </nav>
     )
 }
@@ -34,3 +40,7 @@ export default NavBar;
 // class 이름이 랜덤한 text로 들어가기 때문에 충돌이 일어나지 않는다.
 // 다른 컴포넌트에서도 nav라는 class이름을 사용할 수 있다 !
 // 2-1 ) 두개의 class를 넣고 싶다면 ? 특정 문자열을 만들어서 삽입한다.
+
+// 3. styles jsx
+// 컴포넌트별로 독립되어 있다.
+// 이것도 역시 class이름이 랜덤한 text로 들어감
